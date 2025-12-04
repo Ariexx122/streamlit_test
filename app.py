379e5be2f6cf3642cea7341a6f647d6d238205de
GOD <hernjordan10@gmail.com>
2025-12-04 15:09:05 -0600

    correcting formats

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import time
 import pandas as pd
 import scipy.stats
 import streamlit as st
-import time
 
 # estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
 if 'experiment_no' not in st.session_state:
@@ -47,6 +47,7 @@ if start_button:
                      columns=['no', 'iteraciones', 'media'])
         ],
         axis=0)
-    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
+    st.session_state['df_experiment_results'] = (st.session_state['df_experiment_results']
+                                    .reset_index(drop=True))
 
 st.write(st.session_state['df_experiment_results'])
